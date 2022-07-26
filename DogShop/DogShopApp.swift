07fcec68c51cf0037183d151eda30796e3296450
Akankobateng Rueben <akankobatengr@gmail.com>
2022-07-26 13:24:55 +0000

    Initial Commit

---
+++ DogShop/DogShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogShopApp.swift
+//  DogShop
+//
+//  Created by RUEBEN on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DogShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
